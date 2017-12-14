0ae8c63a9287d5efa31c0347cb106c0634d02d19
MaximeVion <maxime.vion@eemi.com>
2017-12-14 20:15:52 +0100

    messagerie et petit css

--- NeighbourProject/web/assets/js/my-js.js
+++ NeighbourProject/web/assets/js/my-js.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
+    $(".msg-window").scrollTop(1000000);
     $('#openNewUserModal').click( function(){
         $('#newUserModal').modal("show")
     });
     $('#openConnectUserModal').click( function(){
         $('#connectUserModal').modal("show")
     });
+    $('#toolsTable').DataTable({
+        "lengthMenu": [7, 10, 25, 50]
+    });
 
-    $('#toolsTable').DataTable();
 });
